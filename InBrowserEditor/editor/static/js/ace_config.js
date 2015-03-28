071dc01ad174b077b9cc1f5df6d402624a882437
PaulFlorea <paul.florea6@gmail.com>
2015-03-28 17:48:12 -0400

    Auto generating templates

--- InBrowserEditor/editor/static/js/ace_config.js
+++ InBrowserEditor/editor/static/js/ace_config.js
@@ -1,13 +1,36 @@
-
-
 var html_editor ='';
 var css_editor = '';
 $(document).ready(function(){
+
+	$('#save_code').click(save_code);
+	$('#preview').click(function(){location.href=base_url+'preview'});
+
 	html_editor = ace.edit("html_editor");
 	html_editor.setTheme("ace/theme/clouds");
 	html_editor.getSession().setMode("ace/mode/html");
+	html_editor.on('change',function(){$('#save_code').html('Save')});
 
 	css_editor = ace.edit("css_editor");
 	css_editor.setTheme("ace/theme/clouds");
 	css_editor.getSession().setMode("ace/mode/css");
+	css_editor.on('change',function(){$('#save_code').html('Save')});
 });
+
+function save_code(){
+	$object = $(this);
+	$object.prop('disabled',true).html('Saving');
+
+	var formData = new FormData();
+	formData.append('html_code',html_editor.getValue());
+	formData.append('css_code',css_editor.getValue());
+
+	$.ajax({
+		url: base_url+'upload_code/',
+		type: 'POST',
+		data: formData,
+		processData: false,
+		contentType: false,
+		success:function(data){code_uploaded(data,$object)},
+		error:std__error
+	});
+}
--- InBrowserEditor/editor/views.py
+++ InBrowserEditor/editor/views.py
@@ -4,5 +4,33 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = "index.html"
-    
+
+	html_input = ('<!DOCTYPE HTML>'+
+		'\n<html>'+
+		'\n\t<head>'+
+		'\n\t</head>'+
+		'\n\t<body>'+
+		'\n\t\t<h1>Hello World</h1>'+
+		'\n\t</body>'+
+		'\n</html>')
+
+	css_input = ('h1{'+
+		'\n\tcolor:#007092;'+
+		'\n\ttext-align:center;'+
+		'\n\tmargin:60px 0px;'+
+		'\n}')
+
+	template_name = "index.html"
+
+	def get_context_data(self, **kwargs):
+		  """
+		  Populate the context of the template
+		  with all published entries and all the categories.
+		  """
+		  context = super(HomeView, self).get_context_data(**kwargs)
+		  context.update(
+				{'html_input': HomeView.html_input,
+				 'css_input': HomeView.css_input}
+		  )
+		  return context
+
