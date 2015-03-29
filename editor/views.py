8ffbe7afa9170f1aeb8444485da76fee09245f2c
PaulFlorea <paul.florea6@gmail.com>
2015-03-29 05:29:39 -0400

    Fixed dependency bug in heroku

--- editor/views.py
+++ editor/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import Http404, HttpResponse, HttpResponseForbidden, HttpResponseServerError
